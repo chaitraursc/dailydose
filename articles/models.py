3f21128e5e0033f18b5164934a28ff4bd6b536e1
citra <chaitraursc.de@gmail.com>
2025-08-13 14:13:23 +0200

    fix: fixed news description truncation issue

--- articles/models.py
+++ articles/models.py
@@ -20,11 +20,29 @@ class BaseArticle(models.Model):
         
     title = models.CharField(max_length=100, unique=True)
     link = models.URLField()
-    description = models.TextField(max_length=300)
+    description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)  
     source = models.CharField(max_length=100, null=True, blank=True)
     published_at = models.DateField(max_length=100, null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        """
+        Truncates the description to a specified length.
+        """
+        if self.description and len(self.description) > 250:
+            # find the next_period after the 250th character
+            next_period = self.description.find('.', 250)   
+            # if next_period is found, truncate the description at that point
+            # otherwise, truncate at 250 characters         
+            if next_period != -1:
+                self.description = self.description[:next_period + 1]
+            else:
+                self.description = self.description[:250]
+
+
+        # Call the parent class's save method to save the article                   
+        super().save(*args, **kwargs)
+            
     def __str__(self):
         return self.title
 
