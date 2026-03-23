ac75748eaa55b50ed5fb7950c50e658ef163a94a
citra <chaitraursc.de@gmail.com>
2026-03-23 22:07:33 +0100

    fix for style.css file not found by host server

--- dailydose/settings.py
+++ dailydose/settings.py
@@ -152,6 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [STATIC_DIR]
 
 # WhiteNoise configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
